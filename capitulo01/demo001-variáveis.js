c70b28575f7f94f046a1b05b436394e2565e1d11
Bruno-Serafim <br.serafim.27@gmail.com>
2020-07-30 14:57:30 -0300

    Iniciando Meus estudos em fundamentos de javascript com o instrutor Erick Wendel

---
+++ capitulo01/demo001-variáveis.js
@@ -0,0 +1,21 @@
+/*Sobre variáveis
+*var --> não se utiliza mais, ela não significa nada 
+*let --> variáveis que podem se alteradas 
+*const --> variáveis que não podem se alteradas
+*/
+
+let nome = "Super man"
+let idade = 100
+let poder = "Super força"
+
+nome = "Batman"
+poder = "Rico"
+
+console.log(
+    `Nome: ${nome}
+    Idade: ${idade}
+    Poder: ${poder}`
+)
+
+
+
